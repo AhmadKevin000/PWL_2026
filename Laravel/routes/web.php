35a231abae7872fbcfececb64c799201e998eb36
AhmadKevin000 <ahmadkevinmalik01>
2026-02-19 23:38:44 +0700

    2

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,15 +1,30 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
-Route::get('/world}', function () {
+Route::get('/world', function () {
     return 'World';
+});
+
+Route::get('/about', function () {
+    return '244107020125 - Ahmad Kevin Malik Zakaria';
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
