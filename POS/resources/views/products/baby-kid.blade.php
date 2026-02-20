cce54107d9680ad786b414259de57ef963ca8e5b
AhmadKevin000 <ahmadkevinmalik01>
2026-02-20 23:52:34 +0700

    3

---
+++ POS/resources/views/products/baby-kid.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Baby & Kid</title>
+</head>
+<body>
+    <h1>Baby & Kid</h1>
+    <p>Daftar produk bayi dan anak</p>
+
+    <ul>
+        <li>Produk 1</li>
+        <li>Produk 2</li>
+        <li>Produk 3</li>
+    </ul>
+</body>
+</html>
---
+++ POS/resources/views/products/food-beverage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food & Beverage</title>
+</head>
+<body>
+    <h1>Food & Beverage</h1>
+    <p>Daftar produk makanan dan minuman</p>
+
+    <ul>
+        <li>Produk 1</li>
+        <li>Produk 2</li>
+        <li>Produk 3</li>
+    </ul>
+</body>
+</html>
