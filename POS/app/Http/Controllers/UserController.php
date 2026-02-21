1ce850213686775d5c6a9c81738e0c7eed5c6e56
AhmadKevin000 <ahmadkevinmalik01>
2026-02-21 11:26:42 +0700

    4

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function profile($id, $name)
+    {
+        return view('user', ['id' => $id, 'name' => $name]);
+    }
 }
