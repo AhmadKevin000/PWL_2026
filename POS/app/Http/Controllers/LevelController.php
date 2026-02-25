f2685f069d38b7c9c1779ac2d83b8f7dd1c17ac5
AhmadKevin000 <ahmadkevinmalik01>
2026-02-25 07:43:31 +0700

    JS3 4

--- POS/app/Http/Controllers/LevelController.php
+++ POS/app/Http/Controllers/LevelController.php
@@ -9,8 +9,17 @@ class LevelController extends Controller
 {
     public function index()
     {
-        $data = DB::table('m_level')->get();
+        //$data = DB::table('m_level')->get();
         
-        return 'Insert data baru berhasil';
+        //return 'Insert data baru berhasil';
+
+        //$row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Pelanggan', 'CUS']);
+        //return 'Update data berhasil. Jumlah data yang diupdate: '. $row . ' baris';
+
+        //$row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        //return 'Delete data berhasil. Jumlah data yang dihapus: '. $row . ' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }
 }
