58b82bfae51aa88826e314d2743935ef36264982
AhmadKevin000 <ahmadkevinmalik01>
2026-03-03 20:19:40 +0700

    JS5 4

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -23,14 +23,21 @@ class UserController extends Controller
         ];
         
         $activeMenu = 'user';
+
+        $level = LevelModel::all();
         
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level' => $level]);
     }
     
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
         
+        //filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users)
             //menambahkan kolom index
             ->addIndexColumn()
@@ -149,7 +156,7 @@ class UserController extends Controller
         
         try {
             UserModel::destroy($id);
-            
+
             return redirect('/user')->with('success', 'Data user berhasil dihapus');
         } catch (\Throwable $e) {
             report($e);
